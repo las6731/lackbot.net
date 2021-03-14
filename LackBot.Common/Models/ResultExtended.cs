1ce4e36c5fe2967e4220b06c6dfc25afe8f9abb3
Lukas Short <las6731@rit.edu>
2021-03-13 22:21:16 -0400

    Init projects

---
+++ LackBot.Common/Models/ResultExtended.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LackBot.Common.Models
+{
+    public class ResultExtended<T>
+    {
+        public readonly Result Result;
+        public readonly T Value;
+        public readonly string Error;
+
+        public ResultExtended(Result result, T value)
+        {
+            Result = result;
+            Value = value;
+            Error = String.Empty;
+        }
+
+        public ResultExtended(Result result, string error)
+        {
+            Result = result;
+            Value = default;
+            Error = error;
+        }
+
+        public bool IsSuccess() => Result.IsSuccess();
+
+        public static ResultExtended<T> Success(T value) => new ResultExtended<T>(Result.Success, value);
+        
+        public static ResultExtended<T> NoChange(T value) => new ResultExtended<T>(Result.NoChange, value);
+        
+        public static ResultExtended<T> Failure(string error) => new ResultExtended<T>(Result.Failure, error);
+    }
+
+    public enum Result
+    {
+        Success = 0,
+        NoChange = 1,
+        Failure = 2
+    }
+
+    public static class ResultExtensions
+    {
+        public static bool IsSuccess(this Result result)
+        {
+            return result == Result.Success;
+        }
+    } 
+}
---
+++ LackBot.Discord/Models/ConfigData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LackBot.Discord.Models
+{
+    [Serializable]
+    public class ConfigData
+    {
+        public Version Version { get; set; }
+        public string Token { get; set; }
+        
+        public string ApiUrl { get; set; }
+
+        public ConfigData()
+        {
+            Version = Program.AppVersion;
+            Token = "PASTE TOKEN HERE";
+            ApiUrl = "PASTE API URL HERE";
+        }
+    }
+}
